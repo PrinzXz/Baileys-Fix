433f8c870add2a4d8c9eae5a0bf0837280ae4cfd
PrinzXz <chaikalabbror@gmail.com>
2026-02-26 04:32:56 +0700

    style(log): add more info to startup banner

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import makeWASocket from './Socket/index'
 import { DEFAULT_CONNECTION_CONFIG } from './Defaults/index'
 
 const _version = DEFAULT_CONNECTION_CONFIG.version.join('.')
-console.log(`\n[ BAILEYS ] Using Baileys-Fix by PrinzXz ✨ (WA v${_version})\n`)
+console.log(`
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┃ ✨ BAILEYS-FIX BY PRINZXZ
+┃ ➔ WA Version: v${_version}
+┃ ➔ Features: EugenHandler, StickerPack, Carousel
+┃ ➔ Status: 🚀 Library Loaded Successfully
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+`)
 
 export * from '../WAProto/index.js'
 export * from './Utils/index'
